296ecb9ebbdc0e6a49a00e6d6ddbbd5bdbf6d2d4
amitkroutthedev <akrout455@gmail.com>
2023-09-29 19:38:29 +0530

    version 1.0

---
+++ packages/sample-react/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "/src/index.js", // main js
+  output: {
+    path: path.resolve(__dirname, "dist"), // output folder
+    publicPath: "/",
+  },
+  target: 'web',
+  devServer: {
+    port: '5000',
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader" // for styles
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // base html
+    }),
+  ],
+};
